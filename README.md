e4c13b7eaf41f321973b11b7d24ad9d2eda9dee9
Stef De Visscher <sddvissc.devisscher@ugent.be>
2019-04-10 22:30:50 +0200

    intial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Animu
+## heey stef <3'
+
+## heey stien <3 (aka dipshit mcFuckFace ;) )
---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/**
+ * This function is called by the div on the home screen
+ * This will open the users folders to find a torrent file.
+ */
+function open_files(){
+    $('#file-input').click(); // this will click the button that will open the file explorer
+}
